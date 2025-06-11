a27956604a3646d8710de08bd45a15446e81d5f9
amribm <ameerthecoder@gmail.com>
2025-06-11 23:08:26 +0530

    perf: imporved speed by using multicore processing upto 70%

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,12 @@
 use std::collections::BTreeMap;
 use std::fmt::Display;
-use std::io::{BufRead, BufReader};
+use std::io::{BufRead, BufReader, Read, Seek, SeekFrom, Take};
+use std::os::unix::fs::MetadataExt;
 use std::path::Path;
+use std::sync::{Arc, Mutex};
+use std::thread;
 use std::time::Instant;
-use std::{
-    fs,
-    fs::File,
-    io::{Read, Result},
-};
+use std::{fs::File, io::Result};
 
 fn main() {
     let start = Instant::now();
@@ -15,14 +14,88 @@ fn main() {
     let path = Path::new("/Users/ameer/proj/1brc/measurements.txt");
 
     let file = File::open(path).expect("unable to read file");
+    let file_size = file.metadata().unwrap().size();
 
     let reader = BufReader::new(file);
 
-    let shared_map = create_shared_map(reader).unwrap();
+    let available_cpus = std::thread::available_parallelism().unwrap();
 
-    print_shared_map(shared_map);
+    let intervals = get_intervals_for_cpus(available_cpus.into(), file_size, reader).unwrap();
 
-    println!("{:?}", start.elapsed());
+    let mut result = Arc::new(Mutex::new(Vec::new()));
+
+    let mut handles = Vec::new();
+
+    for interval in intervals {
+        let results = Arc::clone(&result);
+
+        let handle = thread::spawn(move || {
+            let station_metrics = process_chunk(interval);
+            results.lock().unwrap().push(station_metrics);
+        });
+
+        handles.push(handle);
+    }
+
+    for handle in handles {
+        handle.join().expect("thread panicked");
+    }
+
+    let result = result
+        .lock()
+        .unwrap()
+        .iter()
+        .fold(StationsMap::default(), |a, b| merge_map(a, &b));
+    print_shared_map(result);
+    println!("\n Execution time: {:?}", start.elapsed());
+}
+
+type StationsMap = BTreeMap<String, City>;
+
+fn process_chunk(interval: Interval) -> StationsMap {
+    let path = Path::new("/Users/ameer/proj/1brc/measurements.txt");
+    let file = File::open(path).expect("unable to read file");
+    let mut reader = BufReader::new(file);
+
+    _ = reader.seek(SeekFrom::Start(interval.start));
+
+    let chunk_reader = reader.take(interval.end - interval.start);
+
+    create_shared_map(chunk_reader).unwrap()
+}
+
+fn get_intervals_for_cpus(
+    cpus: usize,
+    file_size: u64,
+    mut file: BufReader<File>,
+) -> Result<Vec<Interval>> {
+    let mut start = 0;
+    let interval_size = file_size / cpus as u64;
+    let mut intervals = Vec::new();
+    let mut buf = String::new();
+
+    for _ in 0..cpus {
+        let end: u64 = start + interval_size;
+        _ = file.seek(SeekFrom::Start(end));
+
+        let bytes_from_end_to_newline = file.read_line(&mut buf)?;
+        if bytes_from_end_to_newline == 0 {
+            break;
+        }
+        // we dont want to include \n
+        let end = end + (bytes_from_end_to_newline - 1) as u64;
+
+        intervals.push(Interval { start, end });
+        start = end + 1;
+        buf.clear();
+    }
+    Ok(intervals)
+}
+
+#[derive(Debug)]
+struct Interval {
+    start: u64,
+    end: u64,
 }
 
 struct City {
@@ -50,6 +123,13 @@ impl City {
         self.total += temp;
         self.count += 1;
     }
+
+    fn merge(&mut self, other: &Self) {
+        self.min = self.min.min(other.min);
+        self.max = self.max.max(other.max);
+        self.total += other.total;
+        self.count += other.count;
+    }
 }
 
 impl Display for City {
@@ -59,7 +139,7 @@ impl Display for City {
     }
 }
 
-fn create_shared_map(reader: BufReader<File>) -> Result<BTreeMap<String, City>> {
+fn create_shared_map(reader: Take<BufReader<File>>) -> Result<StationsMap> {
     let mut shared_map: BTreeMap<String, City> = BTreeMap::new();
     for line in reader.lines() {
         let line = line?;
@@ -75,7 +155,7 @@ fn create_shared_map(reader: BufReader<File>) -> Result<BTreeMap<String, City>> 
     Ok(shared_map)
 }
 
-fn print_shared_map(map: BTreeMap<String, City>) {
+fn print_shared_map(map: StationsMap) {
     print!("{}", "{");
     for (i, (name, city)) in map.into_iter().enumerate() {
         if i == 0 {
@@ -85,4 +165,13 @@ fn print_shared_map(map: BTreeMap<String, City>) {
         }
     }
     print!("{}", "}");
+}
+
+fn merge_map(a: StationsMap, b: &StationsMap) -> StationsMap {
+    let mut merged_map = a;
+
+    for (k, v) in b {
+        merged_map.entry(k.into()).or_default().merge(v);
+    }
+    merged_map
 }
